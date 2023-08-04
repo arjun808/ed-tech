04d1ec5b372e149aad14cc71722501d1691789f8
Arjun patidar <arjunpatidarprofessional@gmail.com>
2023-08-04 18:15:01 +0530

    First commit

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ Arjun Patidar © 2023 D-learning</div>
         </div>
       </div>
     </div>
